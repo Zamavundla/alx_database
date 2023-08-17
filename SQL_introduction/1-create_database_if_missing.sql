8776e68b28af6e1206dc2bb33a541b17ccde38b9
Zamavundla <zsdouzee8@gmail.com>
2023-08-18 00:57:26 +0200

    .

--- SQL_introduction/1-create_database_if_missing.sql
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -1,2 +1,2 @@
--- Attempt to drop the database (this won't cause an error if it doesn't exist)
-DROP DATABASE IF EXISTS hbtn_0c_0;
+-- Attempt to create the database (this won't cause an error if it already exists)
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;

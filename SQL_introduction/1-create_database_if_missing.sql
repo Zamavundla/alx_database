6878429750ad887c59182f31211471c28be0001f
Zamavundla <zsdouzee8@gmail.com>
2023-08-18 00:51:39 +0200

    .

---
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,7 @@
+-- Check if the database exists
+USE hbtn_0c_0;
+
+-- If the USE statement fails due to the database not existing,
+-- create the database and then switch to it
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
+USE hbtn_0c_0;
